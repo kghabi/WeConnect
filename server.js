3ade9058e73700551aaba78c44f2103bbf2b1f93
kghabi <ghabikarim@gmail.com>
2021-07-29 23:35:53 +0100

    Create routes files

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const connectDB = require('./config/db');
 // inial app with express
-const app=express();
+const app = express();
 
 // Connect DataBase
 connectDB();
 
 app.get('/', (req, res)=> res.send('API Running'));
 
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT= process.env.PORT || 5000;
 
