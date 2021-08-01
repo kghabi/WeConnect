d994f70527080c4c9100737f79b686db4c40c4a1
kghabi <ghabikarim@gmail.com>
2021-08-01 09:21:33 +0100

    Custom Auth middleware and JWT verify

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 // @route    Get api/auth
 // @desc     Test route
 // @access   Public
 
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      res.json(user);
 
-module.exports = router;
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+
+    }
+});
+
+module.exports = router; 
