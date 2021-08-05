8eca629321b7e507fccb45e871b5fc73fb73fa9c
kghabi <ghabikarim@gmail.com>
2021-08-05 14:48:34 +0100

    Create user auth

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,23 @@ import './App.css';
 import React, { Fragment } from 'react';
 import Navbar from './components/styles/Navbar';
 import Home from './components/styles/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 const App = () => (
-  <Fragment>
-    <Navbar/>
-    <Home/>
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Home} />
+      <section className='container'>
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
