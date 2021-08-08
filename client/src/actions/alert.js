2676f41917d331529cc033892b2ab784dfc3160f
kghabi <ghabikarim@gmail.com>
2021-08-08 01:10:27 +0100

    Create auth reducer and the register action

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,7 +1,7 @@
 import { SET_ALERT, REMOVE_ALERT } from './types';
 import { v4 as uuid } from 'uuid';
 
-export const setAlert = (msg, alertType, timeout = 1000) => (dispatch) => {
+export const setAlert = (msg, alertType, timeout = 2500) => (dispatch) => {
   const id = uuid();
   dispatch({
     type: SET_ALERT,
