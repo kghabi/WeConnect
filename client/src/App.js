e2f598c9222086d559b0ba4467ef1dde9c336a8b
kghabi <ghabikarim@gmail.com>
2021-08-08 19:14:47 +0100

    Create the dashboard and the private route for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/styles/Alert';
 import { loadUser } from './actions/auth';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -31,6 +34,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
