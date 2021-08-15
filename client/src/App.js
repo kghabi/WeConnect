7fe54f5188588d3c57e08e8a2672a6408879e115
kghabi <ghabikarim@gmail.com>
2021-08-15 23:06:59 +0100

    Create the post reducer, action and component

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import AddEducation from './components/profile-forms/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 
 //Redux
 import { Provider } from 'react-redux';
@@ -63,6 +64,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
