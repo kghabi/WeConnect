a7d8ef50c9a03f8051f64d696ef6fadf036b8954
kghabi <ghabikarim@gmail.com>
2021-08-16 15:10:00 +0100

    Create the deleting posts

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,5 +1,10 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from '../actions/types';
+import {
+  DELETE_POST,
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+} from '../actions/types';
 
 const initialState = {
   posts: [],
@@ -16,6 +21,12 @@ export default function (state = initialState, action) {
       return {
         ...state,
         posts: payload,
+        loading: false,
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter((post) => post._id !== payload),
         loading: false,
       };
     case POST_ERROR:
